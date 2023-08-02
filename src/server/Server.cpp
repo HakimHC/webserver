31d7f3871e40095f3ebbaf82311d0a4c2f2f0918
HakimHC <ahmedh90111@gmail.com>
2023-08-02 14:25:27 +0200

    Added Server PORT constructor (1 parameter: uint16_t port)

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -33,6 +33,30 @@ Server::Server()
       throw std::runtime_error("fatal: socket cannot listen");
 }
 
+Server::Server(uint16_t port)
+  : _port(port), _name("server1"), _root("www"), _index("index.html"),
+    _maxClientBodySize(4096), _defaultFileDirectory("") {
+
+    this->_allowedMethods.push_back(GET);
+
+    this->_socketFd = socket(AF_INET, SOCK_STREAM, 0);
+    if (this->_socketFd < 0) throw std::runtime_error("fatal: cannot create socket + (" + this->_name + ")");
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(this->_port);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(this->_socketFd, (struct sockaddr*) &addr, sizeof(addr)) < 0)
+      throw std::runtime_error("fatal: cannot bind socket");
+
+    std::cout << "Server listening on 127.0.0.1:8080" << std::endl;
+
+    if (listen(this->_socketFd, _BACKLOG) < 0)
+      throw std::runtime_error("fatal: socket cannot listen");
+}
+
 void Server::print() const {
   std::cout << "== " << this->_name << " ==" << std::endl;
   std::cout << "Port: " << this->_port << std::endl;
