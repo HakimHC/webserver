689d4cdfe281087e103c89a93ad4428bd3e32b46
HakimHC <ahmedh90111@gmail.com>
2023-08-03 14:57:05 +0200

    Working on parsing of the request line. Added a print utility function to the Request class, that prints out all its private attributes. The uri parsing is not finished yet, query strings must be parsed.

--- src/request/Request.cpp
+++ src/request/Request.cpp
@@ -4,6 +4,12 @@
 
 #include "Request.hpp"
 
+enum requestLine {
+  HTTP_METHOD,
+  URI,
+  HTTP_VERSION
+};
+
 Request::Request() {}
 Request::~Request() {}
 
@@ -18,7 +24,32 @@ void Request::getRequestLine(std::string const& buffer) {
   std::string line = buffer.substr(0, crLfPosition);
   std::stringstream tokenizer(line);
   std::string token;
+  int8_t i = 0;
   while (std::getline(tokenizer, token, ' ')) {
-    std::cout << "TOKEN: " << token << std::endl;
+    if (!token.empty()) {
+      switch (i) {
+        case HTTP_METHOD:
+          this->_method = token;
+          break;
+        case URI:
+          this->parseUri(token);
+          break;
+        case HTTP_VERSION:
+          this->_httpVersion = token;
+      }
+      ++i;
+    }
   }
+}
+
+void Request::parseUri(const std::string& uri) {
+  this->_uri = uri;
+}
+
+void Request::print() const {
+  std::cout << "======== REQUEST ========" << std::endl;
+  std::cout << "Method: " << this->_method << std::endl;
+  std::cout << "URI: " << this->_uri << std::endl;
+  std::cout << "Version: " << this->_httpVersion << std::endl;
+  std::cout << "=========================" << std::endl;
 }
